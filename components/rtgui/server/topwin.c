c7f36ad3d804dcbc302de186d67097049847429f
Grissiom <chaos.proton@gmail.com>
2013-07-04 18:14:34 +0800

    check WINTITLE_CLOSEBOX flag before taking action on the close button
    Github-hook: fix #37

--- components/rtgui/server/topwin.c
+++ components/rtgui/server/topwin.c
@@ -1094,7 +1094,9 @@ void rtgui_topwin_title_onmouse(struct rtgui_topwin *win, struct rtgui_event_mou
             }
 #endif
         }
-        else if (win->flag & WINTITLE_CB_PRESSED && event->button & RTGUI_MOUSE_BUTTON_UP)
+        else if (win->flag & WINTITLE_CB_PRESSED &&
+                 event->button & RTGUI_MOUSE_BUTTON_UP &&
+                 win->flag & WINTITLE_CLOSEBOX)
         {
             if (rtgui_rect_contains_point(&rect, event->x, event->y) == RT_EOK)
             {
