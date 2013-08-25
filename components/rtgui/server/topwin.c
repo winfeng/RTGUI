4c081e11464b8e4f78f86a01ab663d2d265f6b95
Grissiom <chaos.proton@gmail.com>
2013-08-25 09:18:03 +0800

    topwin: fix a regression in fcda3a5f887
    When we have multiple window tree and the parent is clipped by others,
    activate child window don't get the parent to be repainted. Define
    RTGUI_ONLY_ONE_WINDOW_TREE if your application only have one window tree
    so we could do some optimization.
    
    Conflicts:
    
    	components/rtgui/server/topwin.c

--- components/rtgui/server/topwin.c
+++ components/rtgui/server/topwin.c
@@ -526,7 +526,12 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
         _rtgui_topwin_raise_tree_from_root(topwin);
         rtgui_topwin_update_clip();
         _rtgui_topwin_draw_tree(
-                _rtgui_topwin_get_root_win(topwin), &epaint);
+#ifdef RTGUI_ONLY_ONE_WINDOW_TREE
+                topwin,
+#else
+                _rtgui_topwin_get_root_win(topwin),
+#endif
+                &epaint);
         return RT_EOK;
     }
 
@@ -552,7 +557,12 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
     _rtgui_topwin_only_activate(topwin);
 
     _rtgui_topwin_draw_tree(
-            _rtgui_topwin_get_root_win(topwin), &epaint);
+#ifdef RTGUI_ONLY_ONE_WINDOW_TREE
+                topwin,
+#else
+                _rtgui_topwin_get_root_win(topwin),
+#endif
+                &epaint);
 
     return RT_EOK;
 }
