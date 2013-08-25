winfeng/RTGUI

commit fcda3a5f8872e9ff7e77bde27917596de4861f31

topwin: show from the activated window in rtgui_topwin_activate_topwin
When topwin A is going to be activate, _draw_tree from A instead of the
root of the topwin. As the parents of A should have already been shown.

Thanks for limxuzheng and hexlog for bug reporting.

Github-hook: fix #39

components/rtgui/server/topwin.c
@@ -525,8 +525,7 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
          */
         _rtgui_topwin_raise_tree_from_root(topwin);
         rtgui_topwin_update_clip();
-        _rtgui_topwin_draw_tree(
-                _rtgui_topwin_get_root_win(topwin), &epaint);
+        _rtgui_topwin_draw_tree(topwin, &epaint);
         return RT_EOK;
     }
 
@@ -551,8 +550,7 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
 
     _rtgui_topwin_only_activate(topwin);
 
-    _rtgui_topwin_draw_tree(
-            _rtgui_topwin_get_root_win(topwin), &epaint);
+    _rtgui_topwin_draw_tree(topwin, &epaint);
 
     return RT_EOK;
 }

commit 518fbee87c2d39facabc9d07eb6db615e3234e5d

topwin: fix a regression in fcda3a5f887
When we have multiple window tree and the parent is clipped by others,
activate child window don't get the parent to be repainted. Define
RTGUI_ONLY_ONE_WINDOW_TREE if your application only have one window tree
so we could do some optimization.

components/rtgui/server/topwin.c
@@ -525,7 +525,13 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
          */
         _rtgui_topwin_raise_tree_from_root(topwin);
         rtgui_topwin_update_clip();
-        _rtgui_topwin_draw_tree(topwin, &epaint);
+        _rtgui_topwin_draw_tree(
+#ifdef RTGUI_ONLY_ONE_WINDOW_TREE
+                topwin,
+#else
+                _rtgui_topwin_get_root_win(topwin),
+#endif
+                &epaint);
         return RT_EOK;
     }
 
@@ -550,7 +556,13 @@ rt_err_t rtgui_topwin_activate_topwin(struct rtgui_topwin *topwin)
 
     _rtgui_topwin_only_activate(topwin);
 
-    _rtgui_topwin_draw_tree(topwin, &epaint);
+    _rtgui_topwin_draw_tree(
+#ifdef RTGUI_ONLY_ONE_WINDOW_TREE
+                topwin,
+#else
+                _rtgui_topwin_get_root_win(topwin),
+#endif
+                &epaint);
 
     return RT_EOK;
 }