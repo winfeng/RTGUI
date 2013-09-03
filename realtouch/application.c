ad8d0bb7d92d6da4b166d92c91a4626cfa9ffbb4
Grissiom <chaos.proton@gmail.com>
2013-09-03 15:52:54 +0800

    realtouch: remove unused code

--- realtouch/application.c
+++ realtouch/application.c
@@ -7,34 +7,7 @@
 
 void init_entry(void *param)
 {
-    rt_device_t device;
-    struct rt_device_rect_info info;
-#ifndef _WIN32
-    rt_device_t device;
-	/* init driver */
-	rt_hw_sdl_start();
-	device = rt_device_find("sdl");
-
-    if (device != RT_NULL)
-    {
-        info.width = 800;
-        info.height = 480;
-        /* set graphic resolution */
-        rt_device_control(device, RTGRAPHIC_CTRL_SET_MODE, &info);
-    }
-
-    rtgui_graphic_set_device(device);
-
-    rtgui_system_server_init();
-
-    /*font system init*/
-    rtgui_font_system_init();
-#endif
     app_mgr_init();
-
-#ifndef _WIN32
-    calibration_init();
-#endif
 }
 
 void rt_application_init(void)
